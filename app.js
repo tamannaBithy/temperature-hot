461c3f4e918c3bb4777b3e80a74ce76ab3819516
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-09-01 20:49:57 +0600

    spinner is added

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
 const searchTemp = async () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    // clear
     searchField.value = '';
+
+    // spinner
+    toggleSpinner('block');
+
+
     const key = '1d08945527b1deba1374e628d42dfa08'
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${key}`
@@ -24,18 +35,28 @@ const searchTemp = async () => {
 // this function is not working inside the display function
 
 const displayTemp = data => {
-    // console.log(data)
+    console.log(data)
+
+    // error handle
+    if (data.cod == 404) {
+        console.log('hello');
+        toggleSpinner('none');
+        alert('city not found!!')
+    }
+
     // destructuring
     const { name, main, weather } = data;
     const { temp_max, temp_min } = main;
     // const { main } = weather[0];
     const status = weather[0].main;
-    console.log(status)
+    // console.log(status)
 
     document.getElementById('location').innerText = name;
     document.getElementById('high-temp').innerText = (temp_max - 273.15).toFixed(2);
     document.getElementById('low-temp').innerText = (temp_min - 273.15).toFixed(2);
     document.getElementById('weather').innerText = status;
+
+    toggleSpinner('none');
 
 
     // condition & changing the icon
